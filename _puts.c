895a8f742f507519fb917bedc0d0a9735d7e4e35
Adanech ALemu <aduti.alemu@gmail.com>
2023-06-30 01:09:32 +0300

    fix _puts.c typo

--- _puts.c
+++ _puts.c
@@ -1,28 +1,6 @@
 #include "main.h"
 
 /**
- * _putchar - prints a character to stdout
- * @c: the character
- *
- * Return: 1 on SUCCESS
- * -1 on fail
- */
-int _putchar(char c)
-{
-	static int count;
-	static char buf[OUTPUT_BUF_SIZE];
-
-	if (c == BUF_FLUSH || count >= OUTPUT_BUF_SIZE)
-	{
-		write(1, buf, count);
-		count = 0;
-	}
-	if (c != BUF_FLUSH)
-		buf[count++] = c;
-	return (1);
-}
-
-/**
  * _puts - prints a string followed by newline
  * @str: the string to be pronted
  * Return: 0
